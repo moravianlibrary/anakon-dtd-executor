19448aff40f4ed73df5f34fe50dac571fedb90e7
Slavik Svyrydiuk <slavik@svyrydiuk.eu>
2025-09-01 14:06:59 +0200

    chore(terraform): add main terraform module

---
+++ main.tf
@@ -0,0 +1,117 @@
+terraform {
+  required_version = "~>1.11"
+  required_providers {
+    # https://registry.terraform.io/providers/kreuzwerker/docker/latest/docs
+    docker = {
+      source  = "kreuzwerker/docker"
+      version = "3.6.2"
+    }
+  }
+}
+
+variable "APP_DB_HOST" {
+  type = string
+}
+
+variable "APP_DB_PORT" {
+  type    = string
+  default = "5432"
+}
+
+variable "APP_DB_NAME" {
+  type = string
+}
+
+variable "APP_DB_USERNAME" {
+  type = string
+}
+
+variable "APP_DB_PASSWORD" {
+  type = string
+}
+
+variable "APP_DYNAMIC_CONFIG_FILE" {
+  type = string
+}
+
+variable "APP_PROCESS_EXECUTION_DIR" {
+  type = string
+}
+
+variable "APP_PROCESS_DEFINITION_DIR" {
+  type = string
+}
+
+variable "docker_host_uri" {
+  type = string
+}
+
+variable "docker_image" {
+  type = string
+}
+
+variable "deploy_domain" {
+  type = string
+}
+
+variable "docker_container_name" {
+  type = string
+}
+
+variable "ghcr_username" {
+  type = string
+}
+
+variable "ghcr_token" {
+  type = string
+}
+
+provider "docker" {
+  host     = var.docker_host_uri
+  ssh_opts = ["-o", "StrictHostKeyChecking=no", "-o", "UserKnownHostsFile=/dev/null"]
+  registry_auth {
+    address  = "ghcr.io"
+    username = var.ghcr_username
+    password = var.ghcr_token
+  }
+}
+
+# Creating anakon_dtd_executor Docker Image
+# with the `latest` as Tag
+resource "docker_image" "anakon_dtd_executor" {
+  name = var.docker_image
+}
+
+# Create Docker Container using the anakon_dtd_executor image.
+resource "docker_container" "anakon_dtd_executor" {
+  count             = 1
+  image             = docker_image.anakon_dtd_executor.image_id
+  name              = var.docker_container_name
+  must_run          = true
+  publish_all_ports = true
+  restart           = "always" # default "no"
+  env = [
+    "APP_DB_HOST=${var.APP_DB_HOST}",
+    "APP_DB_PORT=${var.APP_DB_PORT}",
+    "APP_DB_NAME=${var.APP_DB_NAME}",
+    "APP_DB_USERNAME=${var.APP_DB_USERNAME}",
+    "APP_DB_PASSWORD=${var.APP_DB_PASSWORD}",
+    "APP_PROCESS_EXECUTION_DIR=${var.APP_PROCESS_EXECUTION_DIR}",
+    "APP_PROCESS_DEFINITION_DIR=${var.APP_PROCESS_DEFINITION_DIR}",
+    "APP_DYNAMIC_CONFIG_FILE=${var.APP_DTD_DYNAMIC_CONFIG_FILE}",
+  ]
+
+  mounts {
+    target = var.APP_PROCESS_EXECUTION_DIR
+    type   = "bind"
+    source = "/srv/docker/anakon/${var.APP_ENV_NAME}/process_executions"
+  }
+
+  # provisioner "remote-exec" {
+  #   inline = [
+  #     "mkdir -p /srv/docker/anakon/${var.APP_ENV_NAME}",
+  #     "mkdir -p /srv/docker/anakon/${var.APP_ENV_NAME}/process_definitions",
+  #     "mkdir -p /srv/docker/anakon/${var.APP_ENV_NAME}/process_executions",
+  #   ]
+  # }
+}
